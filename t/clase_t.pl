70cac151f3e4c2006b343997223ea834046a9eca
frmadem <frmadem@gmail.com>
2014-01-26 18:28:48 +0100

    - Added test for the new functionality
    	new file:   t/clase_t.pl

---
+++ t/clase_t.pl
@@ -0,0 +1,36 @@
+package AA;
+
+use lib './lib';
+
+use Eixo::Base::Clase;
+use parent qw(Eixo::Base::Clase);
+
+has (
+
+	n => 55,
+	o => 'aaa',
+	p => {},
+	a => []
+
+);
+
+use strict;
+use warnings;
+
+use Test::More;
+use Data::Dumper;
+use JSON;
+
+my $aa = AA->new;
+
+my @m = qw(n o p a);
+
+is(scalar(@m), scalar(grep { $aa->can($_)} @m), 'Accessors have been created');
+
+is($aa->n, 55, 'Values initialize well (0)');
+is($aa->o, 'aaa', 'Values initialize well (1)');
+is(ref $aa->p, 'HASH', 'Values initialize well (2)');
+is(ref $aa->a, 'ARRAY', 'Values initialize well (3)');
+
+
+done_testing();
