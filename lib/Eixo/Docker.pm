7b77507e3bfe2ba7b3950883f128d4f48e209ad6
J. Gomez <alambike@gmail.com>
2014-03-25 18:07:28 +0100

    Added test of Net::HTTP, Eixo::Rest::Client, Eixo::Base::Clase existence Bump version to 1.0602

--- lib/Eixo/Docker.pm
+++ lib/Eixo/Docker.pm
@@ -7,6 +7,7 @@ use warnings;
 use parent qw(Eixo::Base::Clase);
 use JSON;
 use LWP::UserAgent;
+use Net::HTTP;
 use Eixo::Rest::Client;
 
 
@@ -14,7 +15,7 @@ use Eixo::Rest::Client;
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
-our $VERSION = '1.06';
+our $VERSION = '1.0602';
 
 our $IDENTITY_FUNC = sub {
 
